c50eebc658bf7f4096818697dc1a46de44e30056
Leila-Cl <73179482+Leila-Cl@users.noreply.github.com>
2023-02-24 10:43:26 -0500

    mass push

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './studentApp';
 import "./index.css"
-
-function markPresent(){
-  
-}
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
