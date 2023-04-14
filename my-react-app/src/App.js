d16b92e611e22538d0743b13474719219a314d89
Leila-Cl <73179482+Leila-Cl@users.noreply.github.com>
2023-04-14 11:02:16 -0400

    updates be on time

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -29,27 +29,59 @@ import { studentProf } from './studentDict'
 
 // const timerComponents = [];
 
+const attendanceMark = () =>{
+  const[attendance,setAttendance] = useState(false);
+  const changeAttendance =()=>
+    {setAttendance((prevState)=> !prevState);}
+    {attendance ? "Here":"Not Here"};
+};
+return <button onClick={changeAttendance}>{attendance}</button>
 
+onst[tardy,setTardy] = useState(false);
+  
+  
 function App(props) {
-  
-  
 
   return (
     <div>
+      <div id="cooperProf">
       <div class="studProf">
            {studentProf["Cooper"]["Name"]}
            {studentProf["Cooper"]["Comm"]} 
            {studentProf["Cooper"]["Photo"]}</div>
+          
+          <h2 onClick={()=>setTardy((prevState)=> !prevState)}>
+            Mark as Tardy: {tardy ? "Tardy":"Not Tardy"}</h2>
+            </div>
 
+    <div id="juliaProf">
         <div class="studProf">
           {studentProf["Julia"]["Name"]}
           {studentProf["Julia"]["Comm"]} 
           {studentProf["Julia"]["Photo"]}</div>
+          <h2 onClick={()=>setAttendance((prevState)=> !prevState)}>
+           Mark as Attended: {attendance ? "Here":"Not Here"}
+           </h2>
+          
+          <h2 onClick={()=>setTardy((prevState)=> !prevState)}>
+            Mark as Tardy: {tardy ? "Tardy":"Not Tardy"}</h2>
+            </div>
+        
+        
+        <div id="leilaProf">
         <div class="studProf">
           {studentProf["Leila"]["Name"]}
           {studentProf["Leila"]["Comm"]} 
-          {studentProf["Leila"]["Photo"]}</div>
-    </div>
+          {studentProf["Leila"]["Photo"]}
+          </div>
+          <h2 onClick={()=>setAttendance((prevState)=> !prevState)}>
+           Mark as Attended: {attendance ? "Here":"Not Here"}
+           </h2>
+          
+          <h2 onClick={()=>setTardy((prevState)=> !prevState)}>
+            Mark as Tardy: {tardy ? "Tardy":"Not Tardy"}</h2>
+            </div>
+            </div>
   );
 }
 
